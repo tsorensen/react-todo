dd09daec1e88db3668a7b5ca3e38b5b70e9f3063
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-03-03 09:45:01 -0700

    adds nodemon and todos post route

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 
 import config from 'config';
 import express from 'express';
+import bodyParser from 'body-parser';
 import api from './routes';
 
 const app = express();
 const PORT = config.port;
 
+app.use(bodyParser.json());
 app.use('/api', api);
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
--- routes/todos.js
+++ routes/todos.js
@@ -5,28 +5,34 @@ import express from 'express';
 const { Router } = express;
 const todosRouter = new Router();
 
-const todos = [
-  {
-    name: 'Finish Routes Tutorial',
-    copmleted: false
-  },
-  {
-    name: 'Learn React',
-    completed: false
-  }
-];
+const todos = [];
 
 todosRouter.get('/', (req, res, next) => {
   res.json(todos);
 });
 
+todosRouter.post('/', (req, res, next) => {
+  if(typeof req.body.name !== 'string') {
+    return next(new Error('You must include a `name` field'));
+  }
+  if(typeof req.body.completed !== 'boolean') {
+    req.body.completed = (req.body.completed === 'true') ? true:false;
+  }
+  let todo = {
+    name: req.body.name,
+    completed: req.body.completed
+  };
+  todos.push(todo);
+  res.json(todo);
+});
+
 todosRouter.get('/:id', (req, res, next) => {
   let todo = todos[req.params.id];
   if(!todo) {
-    res.sendStatus(404);
+    return res.status(404).send('Todo not found with given id');
   } else {
     res.json(todo);
   }
-})
+});
 
 export default todosRouter;
