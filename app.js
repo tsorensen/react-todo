39ac38956b11e1bd4270555c66df7cd4ba13d94a
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-03-29 09:06:10 -0600

    adds path to public directory.  exports app for use in automated testing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import path from 'path';
 import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -7,10 +8,14 @@ import api from './routes';
 
 const app = express();
 const PORT = config.port;
+const PUBLIC_DIR = path.join(__dirname, 'public');
 
 app.use(bodyParser.json());
 app.use('/api', api);
+app.use(express.static(PUBLIC_DIR));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+export defualt app;
