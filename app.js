bb8125caaf86f733428102ec4597b22182fe5cb9
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-03-31 09:21:09 -0600

    adds comment

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use('/api', api);
 app.use(express.static(PUBLIC_DIR));
 
+//error handling. Prevents large stack traces in console
 app.use((err, req, res, next) => {
   res.status(500).json({
     error: err.message
