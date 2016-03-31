63e7f7b01b148030bcbf80fa65caf4f25ec43823
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-03-31 08:41:57 -0600

    adds a few more tests

--- test/todos.js
+++ test/todos.js
@@ -47,16 +47,76 @@ describe('todos', () => {
       });
   });
 
-  it('creates a todo')
+  it('creates a todo', () => {
+    return request.post('/api/todos')
+      .send({ name: 'foo', completed: true })
+      .expect(201)
+      .then((res) => {
+        const todo = res.body;
+        expect(todo).to.have.property('name', 'foo');
+        expect(todo).to.have.property('completed', true);
+        expect(todo).to.have.property('id');
+        return Todo.findOneAsync(todo.id);
+      })
+      .then((todo) => {
+        expect(todo).to.have.property('name', 'foo');
+        expect(todo).to.have.property('completed', true);
+      });
+  });
 
-  it('defaults created to false')
+  it('defaults created to false', () => {
+    return request.post('/api/todos')
+      .send({ name: 'foo' })
+      .expect(201)
+      .then((res) => {
+        const todo = res.body;
+        expect(todo).to.have.property('completed', false);
+      });
+  });
 
-  it('updates a todo')
+  it('updates a todo', () => {
+    let id;
+    return Todo.createAsync({ name: 'foo' })
+      .then((todo) => {
+        id = todo.id;
+        expect(todo.name).to.be.equal('foo');
+        return request.put(`/api/todos/${todo.id}`)
+          .send({ name: 'bar' })
+          .expect(200);
+      })
+      .then(() => {
+        return Todo.findOneAsync(id);
+      })
+      .then((todo) => {
+        expect(todo.name).to.be.equal('bar');
+      });
+  });
 
-  it('deletes a todo')
+  it('deletes a todo', () => {
+    let id;
+    return Todo.createAsync({ name: 'bar' })
+      .then((todo) => {
+        id = todo.id;
+        return request.delete(`/api/todos/${todo.id}`)
+          .expect(204);
+      })
+      .then((todo) => {
+        return Todo.findOneAsync(id)
+          .catch((err) => {
+            expect(err).to.match(/not found/);
+          });
+      });
+  });
 
-  it('fails to update if id does not exist')
+  it('fails to update if id does not exist', () => {
+    return request.put('/api/todos/1')
+      .send({ name: 'won\'t update' })
+      .expect(500);
+  });
 
-  it('fails to delete if id does not exist')
+  it('fails to delete if id does not exist', () => {
+    return request.delete('/api/todos/1')
+      .expect(500);
+  });
 
-})
+});
