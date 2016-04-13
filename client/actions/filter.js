5b6149a47c73e7e4c1111a8b48bec7c14be86686
Tyson Sorensen <TysonS@morinda.com>
2016-04-13 13:10:12 -0600

    adds in change visibility action

---
+++ client/actions/filter.js
@@ -0,0 +1,6 @@
+'use strict'
+
+export const setVisibilityFilter = (filter) => ({
+  type: 'CHANGE_VISIBLITY_FILTER',
+  payload: filter
+})
--- client/actions/index.js
+++ client/actions/index.js
@@ -1,3 +1,4 @@
 'use strict'
 
 export * from './todos';
+export * from './filter';
