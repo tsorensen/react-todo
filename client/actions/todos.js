840c567e6921233fffa4b01285c3f2715a7d5e15
Tyson Sorensen <TysonS@morinda.com>
2016-04-13 13:11:09 -0600

    adds rest of todo actions and helper methods

--- client/actions/todos.js
+++ client/actions/todos.js
@@ -1,32 +1,117 @@
 'use strict'
 
-let autoId = 1;
+import request from 'axios'
 
-export const createTodo = (name) => ({
-  type: 'TODO_CREATE',
-  payload: {
-    id: autoId++,
+const createTodoRequest = (todo) => ({
+  type: 'TODO_CREATE_REQUEST',
+  payload: todo
+})
+const createTodoSuccess = (todo) => ({
+  type: 'TODO_CREATE_SUCCESS',
+  payload: todo
+})
+const createTodoFailure = (err) => ({
+  type: 'TODO_CREATE_FAILURE',
+  payload: err,
+  error: true
+})
+
+export const createTodo = (name) => (dispatch) => {
+  const todo = {
     name,
     completed: false
   }
-});
+  dispatch(createTodoRequest(todo))
+  request
+    .post('/api/todos', todo)
+    .then((response) => {
+      dispatch(createTodoSuccess(response.data))
+    })
+    .catch((response) => {
+      dispatch(createTodoFailure(response.data))
+    })
+}
 
-export const updateTodo = (id, partial) => ({
-  type: 'TODO_UPDATE',
-  payload: {
-    id,
-    partial
-  }
-});
-
-export const deleteTodo = (id) => ({
-  type: 'TODO_DELETE',
-  payload: id
-});
-
-export const clearCompleted = (todos) => ({
-  type: 'CLEAR_COMPLETED',
+const listTodosRequest = () => ({
+  type: 'TODO_LIST_REQUEST'
+})
+const listTodosSuccess = (todos) => ({
+  type: 'TODO_LIST_SUCCESS',
   payload: todos
-    .filter((todo) => todo.completed)
-    .map((todo) => todo.id)
-});
+})
+const listTodosFailure = (err) => ({
+  type: 'TODO_LIST_FAILURE',
+  payload: err,
+  error: true
+})
+
+export const listTodos = () => (dispatch) => {
+  dispatch(listTodosRequest())
+  request
+    .get('/api/todos')
+    .then((response) => {
+      dispatch(listTodosSuccess(response.data))
+    })
+    .catch((response) => {
+      dispatch(listTodosFailure(response.data))
+    })
+}
+
+const updateTodoRequest = (id, partial) => ({
+  type: 'TODO_UPDATE_REQUEST',
+  payload: { id, partial }
+})
+const updateTodoSuccess = (todo) => ({
+  type: 'TODO_UPDATE_SUCCESS',
+  payload: todo
+})
+const updateTodoFailure = (err) => ({
+  type: 'TODO_UPDATE_FAILURE',
+  payload: err,
+  error: true
+})
+
+export const updateTodo = (id, update) => (dispatch) => {
+  dispatch(updateTodoRequest(id, update))
+  request
+    .put(`/api/todos/${id}`, update)
+    .then((response) => {
+      dispatch(updateTodoSuccess(response.data))
+    })
+    .catch((response) => {
+      dispatch(updateTodoFailure(response.data))
+    })
+}
+
+const deleteTodoRequest = (id) => ({
+  type: 'TODO_DELETE_REQUEST',
+  payload: id
+})
+const deleteTodoSuccess = (id) => ({
+  type: 'TODO_DELETE_SUCCESS',
+  payload: id
+})
+const deleteTodoFailure = (data) => ({
+  type: 'TODO_DELETE_FAILURE',
+  payload: data,
+  error: true
+})
+
+export const deleteTodo = (id) => (dispatch) => {
+  dispatch(deleteTodoRequest(id))
+  request
+    .delete(`/api/todos/${id}`)
+    .then((response) => {
+      dispatch(deleteTodoSuccess(id))
+    })
+    .catch((response) => {
+      dispatch(deleteTodoFailure(response.data))
+    })
+}
+
+export const clearCompletedTodos = (todos) => (dispatch) => {
+  todos.forEach((todo) => {
+    if (!todo.completed) return
+    todoDelete(todo.id)(dispatch)
+  })
+}
