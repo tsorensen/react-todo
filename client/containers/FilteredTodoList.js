039fffc52d49e6b555a9a1cc3bbcc15b7962eb93
Tyson Sorensen <TysonS@morinda.com>
2016-04-13 13:16:32 -0600

    adds in onTodoDeleteClick

--- client/containers/FilteredTodoList.js
+++ client/containers/FilteredTodoList.js
@@ -1,24 +1,37 @@
-import { connect } from 'react-redux';
-import TodoList from '../components/TodoList';
-import { updateTodo } from '../actions';
+'use strict'
+
+import { connect } from 'react-redux'
+import TodoList from '../components/TodoList'
+import { updateTodo, deleteTodo, listTodos } from '../actions'
 
 const mapStateToProps = (state) => {
-  return {
-    todos: state
-  };
-};
+  let todos = state.todos.items
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onTodoClick(todo) {
-      dispatch(updateTodo(todo.id, {
-        completed: !todo.completed
-      }));
-    }
-  };
-};
+  switch (state.filter) {
+    case 'ACTIVE':
+      todos = todos.filter((todo) => !todo.completed)
+      break;
+    case 'COMPLETED':
+      todos = todos.filter((todo) => todo.completed)
+  }
+
+  return { todos }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  init() {
+    dispatch(listTodos())
+  },
+  onTodoClick(todo) {
+    dispatch(updateTodo(todo.id, { completed: !todo.completed }))
+  },
+  
+  onTodoDeleteClick(todo) {
+    dispatch(deleteTodo(todo.id))
+  }
+})
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(TodoList);
+  mapDispatchToProps
+)(TodoList)
