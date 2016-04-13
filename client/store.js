a2dcda8273064aa0e7170d54848f3a933dc26bf2
Tyson Sorensen <TysonS@morinda.com>
2016-04-13 13:16:12 -0600

    removes stub reducer and adds in our app reducer

--- client/store.js
+++ client/store.js
@@ -1,31 +1,9 @@
 'use strict'
 
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers'
 
-// This is a stubbed reducer. We'll use a real one in a bit
-const reducer = (state = [], action) => {
-  switch(action.type) {
-    case 'TODO_CREATE':
-      return [
-        ...state,
-        action.payload
-      ];
-    case 'TODO_UPDATE':
-      return state.map((todo) => {
-        if(todo.id === action.payload.id) {
-            todo = Object.assing(todo, action.payload.partial);
-        }
-        return todo;
-      });
-    case 'TODO_DELETE':
-      return state.filter((todo) => {
-        return todo.id !== action.payload;
-      });
-    default:
-      return state;
-  }
-};
 
 const devTools = typeof window === 'object'
   && typeof window.devToolsExtension !== 'undefined'
