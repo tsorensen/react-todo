111f6216ca91b634bcd94c925262365e6cf4ee94
Tyson Sorensen <tysonsorensen3@gmail.com>
2016-04-19 09:43:02 -0600

    changes routes to work with mongodb

--- routes/todos.js
+++ routes/todos.js
@@ -1,44 +1,58 @@
-'use strict';
+'use strict'
 
 import express from 'express';
-import Todo from '../lib/todos';
+import Todo from '../models/todo';
 
 const { Router } = express;
 const todosRouter = new Router();
 
-todosRouter.route('/')
-  .get((req, res, next) => {
-    Todo.find((err, todos) => {
-      if(err) { return next(err); }
-      res.json(todos);
-    });
-  })
-  .post((req, res, next) => {
-    Todo.create(req.body, (err, todo) => {
-      if(err) { return next(err); }
-      res.status(201).json(todo);
-    });
-  });
+todosRouter.get('/', (req, res, next) => {
+  Todo.find()
+    .exec()
+    .then((todos) => res.json(todos))
+    .catch(next);
+});
 
-todosRouter.route('/:id')
-  .get((req, res, next) => {
-    Todo.findOne(req.params.id, (err, todo) => {
-      if(err) { return next(err); }
-      if(!todo) { return res.sendStatus(404); }
+todosRouter.post('/', (req, res, next) => {
+  Todo.create(req.body)
+    .then((todo) => res.status(201).json(todo))
+    .catch(next);
+});
+
+todosRouter.get('/:id', (req, res, next) => {
+  Todo.findOne({ _id: req.params.id })
+    .exec()
+    .then((todo) => {
+      if (!todo) {
+        return res.sendStatus(404);
+      }
       res.json(todo);
-    });
-  })
-  .put((req, res, next) => {
-    Todo.update(req.params.id, req.body, (err, todo) => {
-      if(err) { return next(err); }
+    })
+    .catch(next);
+});
+
+todosRouter.put('/:id', (req, res, next) => {
+  Todo
+    .findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true } // Returns the updated document
+    )
+    .exec()
+    .then((todo) => {
+      if (!todo) {
+        return res.sendStatus(404);
+      }
       res.json(todo);
-    });
-  })
-  .delete((req, res, next) => {
-    Todo.delete(req.params.id, (err) => {
-      if(err) { return next(err); }
-      res.sendStatus(204);
-    });
-  });
+    })
+    .catch(next);
+});
+
+todosRouter.delete('/:id', (req, res, next) => {
+  Todo
+    .findOneAndRemove({ _id: req.params.id })
+    .then(() => res.sendStatus(204))
+    .catch(next);
+});
 
 export default todosRouter;
